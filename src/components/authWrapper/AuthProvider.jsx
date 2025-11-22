4afcc2baf1c2bb944c8278a48027a60096ac0ff9
akassem2 <akassem7977@gmail.com>
2025-11-21 20:45:20 -0500

    Used localStorage so that state of login and logout for a user remains after refreshing page

--- src/components/authWrapper/AuthProvider.jsx
+++ src/components/authWrapper/AuthProvider.jsx
@@ -2,14 +2,22 @@ import React, { useState } from "react";
 import AuthContext from "./AuthContext";
 
 export const AuthProvider = ({children}) => {
-    const [user, setUser] = useState(null);
+    //Using localStorage to make sure login state persists throughout the app
+    const [user, setUser] = useState(() => {
+        const previousUser = localStorage.getItem("user");
+        return previousUser ? JSON.parse(previousUser) : null;
+    })
+
 
     const login = (username) => {
-        setUser({username});
+        const newUser = {username};
+        setUser(newUser);
+        localStorage.setItem("user", JSON.stringify(newUser));
     }
 
     const logout = () => {
         setUser(null);
+        localStorage.removeItem("user");
     }
 
     return <AuthContext.Provider value={{
